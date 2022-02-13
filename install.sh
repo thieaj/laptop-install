0ccdc0881c9b3ba93db2e6e96054f661166dc2bb
AJ <andrew@thiesen.co>
2022-02-13 17:00:51 -0500

    remove outdated applications
    Signed-off-by: AJ <andrew@thiesen.co>

--- install.sh
+++ install.sh
@@ -1,79 +1,29 @@
 #!/bin/bash
-# ------------------------------------------------------------------
-# [Author] Taj
-# [Title] MacBook Install Script
-# ------------------------------------------------------------------
-
-#Set Hostname of laptop
-# Make sure you set your hostname here, before running this script.
 sudo scutil --set HostName "HostName"
-
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 # TODO: add wait comment here while homebrew installs
-
 # Check Homebrew for latest version and install
 brew update and upgrade
-
-# Install Brave Browser
-brew install --cask brave-browser
-
 # Install git
 brew install git
-
-# Install GitKraken
-brew install --cask gitkraken
-
 # Install AWS CLI v2
 brew install awscli
-
 # These lines install JetBrains applications. I have a licensed version of these. You may want to comment them out.
 # Install PyCharm
 brew install --cask pycharm
-
 # Install WebStorm
 brew install --cask webstorm
-
 # Install GoLand
 brew install --cask goland
-
 # Install IntelliJ
 brew install --cask intellij-idea
-
 # Install VLC
 brew install VLC
-
 # Install terraform
 brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
-
 # Upgrade Terraform to latest version
 brew upgrade hashicorp/tap/terraform
-
-# Install ansible
-brew install ansible
-
 # Install cloud-nuke
 brew install cloud-nuke
-
-# Install Lastpass
-brew install --cask lastpass
-
-# Install Setapp
-brew install --cask setapp
-
-# Install PocketCasts
-brew install --cask pocket-casts
-
-# Install Zoom
-brew install --cask zoom
-
-# Install Go Lang
-brew install go
-
-# Install NodeJS
-brew install node
-
-# Install Audacity
-brew install --cask audacity
